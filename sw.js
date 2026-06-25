a8f0114c97ae91cf8e0ba1424d860264ebb7dc9a
pagaremoto <paga7834@gmail.com>
2026-06-25 14:23:36 +0100

    Primer commit

---
+++ sw.js
@@ -0,0 +1,5 @@
+self.addEventListener('fetch', function(event) {
+    // Requisito para activar el modo App en el móvil
+});self.addEventListener('fetch', function(event) {
+    // Requisito para activar el modo App en el móvil
+});
